c809150e670b19472d1a3ec7c6c0640ce290a305
Carlos Ramos <carlosramosweb@hotmail.com>
2016-04-02 17:30:09 -0300

    Arquivo de inicialização para plugin Wordpress
    Está modelo é quase padrão e obrigatório para plugins Wordpress. Nele podemos criar os vínculos para todas as condições do sistema.

---
+++ wp-reserve-laboratory.php
@@ -0,0 +1,17 @@
+<?php
+/*---------------------------------------------------------
+Plugin Name: Wp Reserve Laboratory
+Plugin URI: https://wordpress.org/plugins/wp-reserve-laboratory/
+Author: Naiara Ramos, 
+Author URI: http://facsenac.com.br
+Description: Trabalho de faculdade, destinado a reservas de laboratórios do Campus Senac 903 sul
+Text Domain:  wp-reserve-laboratory
+Domain Path: /languages
+Version: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html 
+------------------------------------------------------------*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Sair se for acessado diretamente.
+}
